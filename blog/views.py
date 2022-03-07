4ba4e3dee8b5c981de0876771546e76cd20be692
oni007 <o.ahmadi1992@gmail.com>
2022-03-07 15:01:20 +0330

    Persian Number

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Article, Category
 
 
 def home(request):
+    article_list = Article.objects.published()
+    paginator = Paginator(article_list, 2)  # Show 5 article per page.
+    page = request.GET.get('page')
+    articles = paginator.get_page(page)
     context = {
-        "articles": Article.objects.published()
+        "articles": articles
     }
     return render(request, 'blog/home.html', context)
 
